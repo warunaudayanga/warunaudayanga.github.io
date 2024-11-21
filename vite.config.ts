be76afc8ed706369fa7462ea2e47d9562e8aca33
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2024-11-21 14:38:40 +0530

    Improve

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,41 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { analyzer } from "vite-bundle-analyzer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
     base: "./",
     server: {
-        port: 3000, // Optional: choose your preferred port
+        port: 3000,
     },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    // Separate React and React DOM into a separate chunk
+                    if (id.includes("node_modules/react")) {
+                        return "react";
+                    }
+
+                    // Separate Firebase into its own chunk
+                    if (id.includes("node_modules/firebase")) {
+                        return "firebase";
+                    }
+
+                    // Other large dependencies can be chunked similarly
+                },
+            },
+        },
+    },
+    optimizeDeps: {
+        include: [
+            "react",
+            "react-dom",
+            "firebase",
+            "zustand",
+            // Add any other libraries you want to optimize
+        ],
+    },
+    plugins: [react(), analyzer()],
 });
