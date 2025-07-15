1d66cc7013251e212585e770b7f5d88bde4c375e
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2025-07-16 01:10:56 +0530

    Enhance DialogBackdrop padding for improved layout consistency and spacing.

--- src/components/layout/Dialog/DialogBackdrop.tsx
+++ src/components/layout/Dialog/DialogBackdrop.tsx
@@ -5,7 +5,7 @@ const DialogBackdrop = ({ children }: PropsWithChildren): JSX.Element => {
 
     return (
         <div
-            className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-[1001]"
+            className="fixed top-0 left-0 p-5 right-0 bottom-0 flex items-center justify-center z-[1001]"
             style={{ backgroundColor: `rgba(0, 0, 0, ${BACKDROP_OPACITY})` }}
         >
             {children}
